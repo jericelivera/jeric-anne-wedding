2a7b577a04959bb3b47bff2318c063a03c44cc93
jericelivera <jericelivera@gmail.com>
2026-07-20 21:21:27 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -173,3 +173,10 @@ rsvpForm.addEventListener("submit", function (e) {
 });
 
 });
+const faqs = document.querySelectorAll('.faq-item');
+
+faqs.forEach(item=>{
+    item.querySelector('.faq-question').addEventListener('click',()=>{
+        item.classList.toggle('active');
+    });
+});
